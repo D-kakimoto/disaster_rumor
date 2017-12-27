7115e7bd04df41351481d10228efd7368efba5be
dkakimoto <shinra.dai0419@gmail.com>
2017-12-27 15:27:10 +0900

    キーワードリストを取得

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-test
+<!DOCTYPE html>
+<html lang="ja">
+  <title>災害時の流言</title>
+  <head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+    <script type="text/javascript" src="js/sample.js">//</script>
+  </head>
+  <body>
+    <?php
+      //キーワードリストの読み込み
+      $filename = 'keywordlist.txt';
+      $fp = fopen($filename, 'r');
+      $keywords = fgets($fp);
+      fclose($fp);
+
+      //アンダーバーで区切って配列に格納
+      $keyword = explode("_", $keywords);
+
+      //空要素を削除し，添字を詰める
+      $keyword = array_filter($keyword, "strlen");
+      $keyword = array_values($keyword);
+
+      for($i=0;$i<=count($keyword);$i++){
+        echo $keyword[$i].'<br>';
+      }
+
+      echo '<script type="text/javascript">';
+      echo 'alert("'.$keyword[1].'");';
+      echo '</script>';
+      ?>
+  </body>
+</html>
